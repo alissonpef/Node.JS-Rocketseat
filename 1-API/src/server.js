f15b28d09cf21a739a4f4eab4b17db831cf1ef49
Alisson <alissonpef@gmail.com>
2025-10-10 17:04:34 -0300

    fix: uncomment method assignment for clarity in server.js

--- 1-API/src/server.js
+++ 1-API/src/server.js
@@ -1,7 +1,7 @@
 import http from "node:http"
 
 const server = http.createServer((req, res) => {
-    const { method } = req
+    const { method } = req // const method = req.method;
     return res.end("Método: " + method) 
 })
 
