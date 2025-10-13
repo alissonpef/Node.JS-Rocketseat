9b85d1d66ac1a47d2b5fdaca81a11c4de1e019ab
Alisson <alissonpef@gmail.com>
2025-10-13 14:01:59 -0300

    fix: update server response to include HTTP status code in server.js

--- 1-API/src/server.js
+++ 1-API/src/server.js
@@ -2,7 +2,7 @@ import http from "node:http"
 
 const server = http.createServer((req, res) => {
     const { method } = req // const method = req.method;
-    return res.end("Método: " + method) 
+    return res.writeHead(200).end("Método: " + method) 
 })
 
 server.listen(8080)
