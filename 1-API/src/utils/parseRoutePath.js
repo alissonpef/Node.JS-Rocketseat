cd66a8077bde2375a5839119d0b0830ad82fc593
Alisson <alissonpef@gmail.com>
2025-10-20 17:14:32 -0300

    fix: update parseRoutePath to include query string handling in regex

--- 1-API/src/utils/parseRoutePath.js
+++ 1-API/src/utils/parseRoutePath.js
@@ -1,10 +1,10 @@
 export function parseRoutePath(path) {
-  // Pega o que vem em /:id
+  // Pega o que vem em /:id (parametro não nomeado)
   const routeParametersRegex = /:([a-zA-Z]+)/g;
 
   const params = path.replaceAll(routeParametersRegex, "(?<$1>[a-z0-9-_]+)");
   // Objeto
-  const pathRegex = new RegExp(params);
+  const pathRegex = new RegExp(`${params}(?<query>\\?(.*))?$`);
 
   return pathRegex;
 }
