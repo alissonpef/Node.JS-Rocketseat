c4a75630e65ad968254e03ca865071ac74e36738
Alisson <alissonpef@gmail.com>
2025-10-20 14:01:44 -0300

    refactor: simplify request handling by delegating routing to routeHandler

--- 1-API/src/server.js
+++ 1-API/src/server.js
@@ -1,22 +1,10 @@
 import http from "node:http";
 import { jsonBodyHandler } from "./middlewares/jsonBodyHandler.js";
+import { routeHandler } from "./middlewares/routeHandler.js";
 
 const server = http.createServer(async (req, res) => {
-  const { method, url } = req; // const method = req.method;
-
   await jsonBodyHandler(req, res);
-
-  if (method === "GET" && url === "/products") {
-    return res.end("Lista de Produtos!");
-  }
-
-  if (method === "POST" && url === "/products") {
-    // Ele vai dar erro se colocar req.body, pois o req retorna strings e nṹmeros, porém, ele printa apenas strings
-    // Caso queira printar um tipo númerico especifico, daria para fazer req.body.price.toString()
-    return res.writeHead(201).end(JSON.stringify(req.body));
-  }
-
-  return res.writeHead(404).end("Rota não encontrada!");
+  routeHandler(req, res);
 });
 
 server.listen(8080);
