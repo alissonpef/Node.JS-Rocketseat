8c932eca2f795465c5a46e3763a4ca28f226a27c
Alisson <alissonpef@gmail.com>
2025-10-20 17:50:39 -0300

    refactor: update route controllers to use destructured parameters and database access

--- 1-API/src/routes.js
+++ 1-API/src/routes.js
@@ -4,22 +4,27 @@ export const routes = [
   {
     method: "GET",
     path: "/products",
-    controller: (req, res) => {
-      console.log(req.query);
-      return res.end(JSON.stringify(req.query));
+    controller: ({ req, res, database }) => {
+      const products = database.select("products");
+
+      return res.end(JSON.stringify(products));
     },
   },
   {
     method: "POST",
     path: "/products",
-    controller: (req, res) => {
+    controller: ({ req, res, database }) => {
+      const { name, price } = req.body;
+
+      database.insert("products", { name, price });
+
       return res.writeHead(201).end(JSON.stringify(req.body));
     },
   },
   {
     method: "DELETE",
     path: "/products/:id",
-    controller: (req, res) => {
+    controller: ({ req, res }) => {
       return res
         .writeHead(201)
         .end("Produto removido com ID: " + req.params.id);
