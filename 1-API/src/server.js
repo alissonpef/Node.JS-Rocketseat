89de5e912c3d8ceefd34f42ed1a2650257511a97
Alisson <alissonpef@gmail.com>
2025-10-20 10:11:04 -0300

    feat: refactor server request handling to support async operations for POST requests

--- 1-API/src/server.js
+++ 1-API/src/server.js
@@ -1,6 +1,6 @@
 import http from "node:http"
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const { method, url } = req // const method = req.method;
 
     if (method === "GET" && url === "/products") {
@@ -8,6 +8,14 @@ const server = http.createServer((req, res) => {
     }
 
     if (method === "POST" && url === "/products") {
+        const buffers = []
+
+        for await (const chunk of req) {
+            buffers.push(chunk)
+        }
+        console.log(buffers)
+        console.log(Buffer.concat(buffers).toString())
+
         return res.writeHead(201).end("Produto cadastrado!")
     }
 
